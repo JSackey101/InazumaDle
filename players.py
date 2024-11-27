083f46c71a4777fa955bde5734ce0632ff9d75c5
JSackey101 <jeffsackey101@gmail.com>
2024-11-27 11:15:34 +0000

    Commented out the creation of the attributes other than the player_dict attribute for the Player class as I am unsure whether I need the other attributes as of right now

--- players.py
+++ players.py
@@ -3,7 +3,7 @@ class Player:
         self.player_dict = {}
         for i in range(len(header_info)):
             self.player_dict[header_info[i]] = player_data[i]
-            exec(f"self.{header_info[i]} = player_data[i]")
+            # exec(f"self.{header_info[i]} = player_data[i]")
 
 
 class PlayerDatabase:
