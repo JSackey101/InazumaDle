b771893b5a74f6b0820954a86232b46fa8fb7fd5
JSackey101 <jeffsackey101@gmail.com>
2024-12-03 12:14:37 +0000

    Added a tries input to the make_guess function in the main.py file which would allow for the input of the current number of attempts. Added code functionality to increment the number of tries by 1 per guess and added a return of this value.
    Moved the csv file reading, random player generation and tries variable creation to outside of the prog_start loop as I do not want this to be done multiple times.
    
    When the user types Guess for the initial prompt, the make_guess function's output will update the tries variable alongside adding to the correct_guess variable and a print is displayed when the correct_guess variable is True, representing a correct guess.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import random
 #   player_data.player_obj_list[1])))
 # player_data.print_players("s")
 
-def make_guess(player_data, random_player):
+def make_guess(player_data, random_player, tries):
     guess_not_made = True
     while guess_not_made:
         guess_name = input_checker(
@@ -22,7 +22,8 @@ def make_guess(player_data, random_player):
         elif len(matches) == 1:
             correct_guess = player_data.comparison_result(
                 random_player, matches[0])
-            return correct_guess
+            tries += 1
+            return correct_guess, tries
 
         else:
             if guess_name.split(" ") > 1:
@@ -49,22 +50,32 @@ def make_guess(player_data, random_player):
                     else:
                         correct_guess = player_data.comparison_result(
                             random_player, matches[0])
-                        return correct_guess
+                        tries += 1
+                        return correct_guess, tries
 
 
 if __name__ == "__main__":
+    player_data = PlayerDatabase(read_player_data("data.csv"))
+    random_player = player_data.player_obj_list[random.randint(
+        0, player_data.player_count - 1)]
+    tries = 0
     prog_start = True
     while prog_start:
-        player_data = PlayerDatabase(read_player_data("data.csv"))
-        random_player = player_data.player_obj_list[random.randint(
-            0, player_data.player_count - 1)]
-        tries = 0
         prompt = two_input_checker("""\nWhat would you like to do?
 \nType "Guess" to make a guess.
 \nType "Check" to see a list of characters beginning with a specific letter.
 \nEnter here: """,
                                    """\nYou must type either "Guess" or "Check".""", "Guess", "Check")
         if prompt == 1:
-            correct_guess = make_guess(player_data, random_player)
+            correct_guess, tries = make_guess(
+                player_data, random_player, tries)
+            if correct_guess:
+                print(f"""Victory!
+                      
+You guessed {random_player}
+
+Number of tries: {tries}""")
+                break
+
         else:
             pass
