f36e9974a34051e375117b5a8abcb7289c5c0d1a
JSackey101 <jeffsackey101@gmail.com>
2024-12-14 23:11:07 +0000

    Moved the check_players function to the utilities.py file.
    Updated the function import in the main.py file to import this function. Removed the input_checker function import from the main.py file as the main code was not making use of this.

--- main.py
+++ main.py
@@ -1,33 +1,11 @@
 from players import Player, PlayerDatabase
-from utilities import read_player_data, input_checker, two_input_checker, make_guess
+from utilities import read_player_data, two_input_checker, make_guess, check_players
 import random
 
 
 # print(list(player_data.player_obj_list[0].compare_players(
 #   player_data.player_obj_list[1])))
 # player_data.print_players("s")
-
-def check_players(player_data, guessed_players):
-    check_not_done = True
-    while check_not_done:
-        check_letter = input_checker(
-            "Enter the letter you wish to find character matches for: ", str,
-            "This input should only contain letters.")
-        if len(check_letter) > 1:
-            print("You should only enter 1 letter\n")
-            continue
-        matches = [player for player in player_data.player_obj_list if (player.player_dict["name"][0] == check_letter.lower()
-                   or player.player_dict["nickname"][0] == check_letter.lower()) and player not in guessed_players]
-        if len(matches) == 0:
-            print(
-                f"""No player names/nicknames that have not been guessed begin with the letter: (\"{check_letter.upper()}\").
-                    \n""")
-        else:
-            print(
-                f"\nThe characters that have not been guessed beginning with {check_letter.upper()} are:")
-            for player in matches:
-                print(player)
-            check_not_done = False
 
 
 if __name__ == "__main__":
--- utilities.py
+++ utilities.py
@@ -98,3 +98,26 @@ def make_guess(player_data, random_player, tries, guessed_players):
                             guessed_players.append(matches[0])
                             tries += 1
                             return correct_guess, tries
+
+
+def check_players(player_data, guessed_players):
+    check_not_done = True
+    while check_not_done:
+        check_letter = input_checker(
+            "Enter the letter you wish to find character matches for: ", str,
+            "This input should only contain letters.")
+        if len(check_letter) > 1:
+            print("You should only enter 1 letter\n")
+            continue
+        matches = [player for player in player_data.player_obj_list if (player.player_dict["name"][0] == check_letter.lower()
+                   or player.player_dict["nickname"][0] == check_letter.lower()) and player not in guessed_players]
+        if len(matches) == 0:
+            print(
+                f"""No player names/nicknames that have not been guessed begin with the letter: (\"{check_letter.upper()}\").
+                    \n""")
+        else:
+            print(
+                f"\nThe characters that have not been guessed beginning with {check_letter.upper()} are:")
+            for player in matches:
+                print(player)
+            check_not_done = False
