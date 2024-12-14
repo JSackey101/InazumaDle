6aae2d3cef824ceef825f54353d16fd2d3ce4603
JSackey101 <jeffsackey101@gmail.com>
2024-12-14 23:09:08 +0000

    Made changes to the check_players function in the main.py file to only showcase players that have not been guessed already to try and avoid repeat guesses.
    Updated the function call to reflect this.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import random
 #   player_data.player_obj_list[1])))
 # player_data.print_players("s")
 
-def check_players(player_data):
+def check_players(player_data, guessed_players):
     check_not_done = True
     while check_not_done:
         check_letter = input_checker(
@@ -16,15 +16,15 @@ def check_players(player_data):
         if len(check_letter) > 1:
             print("You should only enter 1 letter\n")
             continue
-        matches = [player for player in player_data.player_obj_list if player.player_dict["name"][0] == check_letter.lower()
-                   or player.player_dict["nickname"][0] == check_letter.lower()]
+        matches = [player for player in player_data.player_obj_list if (player.player_dict["name"][0] == check_letter.lower()
+                   or player.player_dict["nickname"][0] == check_letter.lower()) and player not in guessed_players]
         if len(matches) == 0:
             print(
-                f"""No names/nicknames begin with the letter: (\"{check_letter.upper()}\").
+                f"""No player names/nicknames that have not been guessed begin with the letter: (\"{check_letter.upper()}\").
                     \n""")
         else:
             print(
-                f"\nThe characters beginning with {check_letter.upper()} are:")
+                f"\nThe characters that have not been guessed beginning with {check_letter.upper()} are:")
             for player in matches:
                 print(player)
             check_not_done = False
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     while prog_start:
         prompt = two_input_checker("""\nWhat would you like to do?
 \nType "Guess" to make a guess.
-\nType "Check" to see a list of characters beginning with a specific letter.
+\nType "Check" to see a list of characters beginning with a specific letter that have not been guessed already.
 \nEnter here: """,
                                    """\nYou must type either "Guess" or "Check".""", "Guess", "Check")
         if prompt == 1:
@@ -55,4 +55,4 @@ Number of tries: {tries}""")
                 break
 
         else:
-            check_players(player_data)
+            check_players(player_data, guessed_players)
