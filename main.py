e521f7012b8ac53171a7514bfb225702330108d2
JSackey101 <jeffsackey101@gmail.com>
2024-12-15 03:11:29 +0000

    Removed commented code I decided I would not need anymore.

--- main.py
+++ main.py
@@ -3,11 +3,6 @@ from utilities import read_player_data, two_input_checker, make_guess, check_pla
 import random
 
 
-# print(list(player_data.player_obj_list[0].compare_players(
-#   player_data.player_obj_list[1])))
-# player_data.print_players("s")
-
-
 if __name__ == "__main__":
     player_data = PlayerDatabase(read_player_data("data.csv"))
     random_player = player_data.player_obj_list[random.randint(
