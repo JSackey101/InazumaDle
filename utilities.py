ff1c5f76e614f4fb08fef0d5d532e686849ed60f
JSackey101 <jeffsackey101@gmail.com>
2024-12-21 19:44:37 +0000

    Added console input to the check_players function and changed the function to use the console object for inputs and prints

--- utilities.py
+++ utilities.py
@@ -101,26 +101,26 @@ def make_guess(player_data, random_player, tries, guessed_players, console):
                             return correct_guess, tries
 
 
-def check_players(player_data, guessed_players):
+def check_players(player_data, guessed_players, console):
     check_not_done = True
     while check_not_done:
         check_letter = input_checker(
             "Enter the letter you wish to find character matches for: ", str,
-            "This input should only contain letters.")
+            "This input should only contain letters.", console)
         if len(check_letter) > 1:
-            print("You should only enter 1 letter\n")
+            console.print("You should only enter 1 letter\n")
             continue
         matches = [player for player in player_data.player_obj_list if (player.player_dict["name"][0] == check_letter.lower()
                    or player.player_dict["nickname"][0] == check_letter.lower()) and player not in guessed_players]
         if len(matches) == 0:
-            print(
+            console.print(
                 f"""No player names/nicknames that have not been guessed begin with the letter: (\"{check_letter.upper()}\").
                     \n""")
         else:
-            print(
+            console.print(
                 f"\nThe characters that have not been guessed beginning with {check_letter.upper()} are:")
             for player in matches:
-                print(player)
+                console.print(player)
             check_not_done = False
 
 
