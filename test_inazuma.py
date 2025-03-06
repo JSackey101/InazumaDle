24085c009e7431acf70377d8cdfd2aef09676c49
JSackey101 <jeffsackey101@gmail.com>
2025-03-06 14:18:37 +0000

    Updated tests

--- test_inazuma.py
+++ test_inazuma.py
@@ -4,29 +4,42 @@ from io import StringIO
 from players import Player
 from utilities import read_player_data, ErrorRaising
 
-def test_read_player_data(test_player_data):
-    """ Tests whether the list of Player objects is created correctly. """
-    with patch("builtins.open", mock_open(read_data=test_player_data)):
-        players = read_player_data("test.csv")
-        assert isinstance(players, list)
-        assert all(isinstance(player, Player) for player in players)
-        assert len(players) == 2
+class TestErrorRaising():
+    """ A class to test the ErrorRaising class. """
 
-def test_validate_str():
-    """ Tests whether a TypeError is raised when a non string is input. """
-    with pytest.raises(TypeError):
-        ErrorRaising.validate_str(10)
+    @staticmethod
+    def test_read_player_data(test_player_data):
+        """ Tests whether the list of Player objects is created correctly. """
+        with patch("builtins.open", mock_open(read_data=test_player_data)):
+            players = read_player_data("test.csv")
+            assert isinstance(players, list)
+            assert all(isinstance(player, Player) for player in players)
+            assert len(players) == 2
 
+    @staticmethod
+    def test_validate_str():
+        """ Tests whether a TypeError is raised when a non string is input. """
+        with pytest.raises(TypeError):
+            ErrorRaising.validate_str(10)
 
-def test_validate_char_space():
-    """ Tests whether a ValueError is raised when an input string is not
-        only characters and spaces. """
-    with pytest.raises(ValueError):
-        ErrorRaising.validate_char_space("test 1")
+    @staticmethod
+    def test_validate_char_space():
+        """ Tests whether a ValueError is raised when an input string is not
+            only characters and spaces. """
+        with pytest.raises(ValueError):
+            ErrorRaising.validate_char_space("test 1")
 
+    @staticmethod
+    def test_validate_digits():
+        """ Tests whether a ValueError is raised when an input string is not
+            only digits. """
+        with pytest.raises(ValueError):
+            ErrorRaising.validate_digits("12four5")
 
-def test_validate_digits():
-    """ Tests whether a ValueError is raised when an input string is not
-        only digits. """
-    with pytest.raises(ValueError):
-        ErrorRaising.validate_digits("12four5")
+    @staticmethod
+    def test_validate_two_inputs():
+        """ Tests whether a ValueError is raised when an input string is not 
+            either of 2 inputs. """
+        with pytest.raises(ValueError):
+            ErrorRaising.validate_two_inputs("test", "True", "False")
+
