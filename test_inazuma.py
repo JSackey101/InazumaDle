cb815f9aaea7569494cf1f17c705d2e71dd03ed0
JSackey101 <jeffsackey101@gmail.com>
2025-03-06 13:09:40 +0000

    Added tests.

--- test_inazuma.py
+++ test_inazuma.py
@@ -2,7 +2,7 @@ import pytest
 from unittest.mock import patch, mock_open, MagicMock
 from io import StringIO
 from players import Player
-from utilities import read_player_data
+from utilities import read_player_data, ErrorRaising
 
 def test_read_player_data(test_player_data):
     """ Tests whether the list of Player objects is created correctly. """
@@ -11,3 +11,15 @@ def test_read_player_data(test_player_data):
         assert isinstance(players, list)
         assert all(isinstance(player, Player) for player in players)
         assert len(players) == 2
+
+def test_validate_str():
+    """ Tests whether a TypeError is raised when a non string is input. """
+    with pytest.raises(TypeError):
+        ErrorRaising.validate_str(10)
+
+
+def test_validate_char_space():
+    """ Tests whether a ValueError is raised when an input string is not
+        only characters and spaces. """
+    with pytest.raises(ValueError):
+        ErrorRaising.validate_char_space("test 1")
