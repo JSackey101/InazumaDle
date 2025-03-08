dba10f1430a7e364ec5a181ab751b3cbb795b0f3
JSackey101 <jeffsackey101@gmail.com>
2025-03-08 20:20:31 +0000

    Added tests.

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from unittest.mock import MagicMock
 from rich.console import Console
+from players import Player
 
 @pytest.fixture
 def test_player_data():
@@ -13,3 +14,38 @@ kazemaru,kazemaru ichirouta,wind,male,DF,2"""
 def test_console():
     """ Returns a Mock Console object. """
     return MagicMock(spec=Console)
+
+@pytest.fixture()
+def test_players_no_dupes():
+    """ Returns a list of Player objects with no duplicate values. """
+    header_info = "nickname,name,element,gender,position,year".split(",")
+    player_one = "shourin,shourinji ayumu,forest,male,MF,1".split(",")
+    player_two = "shishido,shishido sakichi,fire,male,MF,1".split(",")
+    return [Player(player, header_info) for player in (player_one, player_two)]
+
+
+@pytest.fixture()
+def test_players_same_nick():
+    """ Returns a list of Player objects with the same nickname. """
+    header_info = "nickname,name,element,gender,position,year".split(",")
+    player_one = "shourin,shourinji ayumu,forest,male,MF,1".split(",")
+    player_two = "shourin,shishido sakichi,fire,male,MF,1".split(",")
+    return [Player(player, header_info) for player in (player_one, player_two)]
+
+
+@pytest.fixture()
+def test_players_same_first():
+    """ Returns a list of Player objects with the same first name. """
+    header_info = "nickname,name,element,gender,position,year".split(",")
+    player_one = "shourin,shourinji ayumu,forest,male,MF,1".split(",")
+    player_two = "shishido,shourinji sakichi,fire,male,MF,1".split(",")
+    return [Player(player, header_info) for player in (player_one, player_two)]
+
+
+@pytest.fixture()
+def test_players_same_last():
+    """ Returns a list of Player objects with the same last name. """
+    header_info = "nickname,name,element,gender,position,year".split(",")
+    player_one = "shourin,shourinji ayumu,forest,male,MF,1".split(",")
+    player_two = "shishido,shishido ayumu,fire,male,MF,1".split(",")
+    return [Player(player, header_info) for player in (player_one, player_two)]
--- test_inazuma.py
+++ test_inazuma.py
@@ -43,6 +43,24 @@ class TestErrorRaising():
         with pytest.raises(ValueError):
             ErrorRaising.validate_two_inputs("test", "True", "False")
 
+    @staticmethod
+    def test_validate_one_name():
+        """ Tests whether a ValueError is raised when input string contains more than 1 name. """
+        with pytest.raises(ValueError):
+            ErrorRaising.validate_one_name("jeffrey sackey")
+
+    @staticmethod
+    def test_validate_empty_string():
+        """ Tests whether a ValueError is raised when input string is empty. """
+        with pytest.raises(ValueError):
+            ErrorRaising.validate_not_empty("")
+    
+    @staticmethod
+    def test_validate_whitespace_string():
+        """ Tests whether a ValueError is raised when input string is only whitespace. """
+        with pytest.raises(ValueError):
+            ErrorRaising.validate_not_empty("   ")
+
 
 class TestUtility():
     """ A class to test the Utility class. """
@@ -82,3 +100,95 @@ class TestUtility():
         """ Tests whether 2 is returned when the input matches the second option. """
         option = Utility.check_two_options("False", "True", "False")
         assert option == 2
+
+    @staticmethod
+    def test_search_first(test_players_no_dupes):
+        """ Tests whether a list with the correct Player object 
+            is returned for a matching first name. """
+        matches = Utility.search_first_nick_name(test_players_no_dupes, "shourinji")
+        assert isinstance(matches, list)
+        assert len(matches) == 1
+        assert matches[0] == test_players_no_dupes[0]
+
+    @staticmethod
+    def test_search_first_case(test_players_no_dupes):
+        """ Tests whether a list with the correct Player object is 
+            returned for a matching first name regardless of case. """
+        matches = Utility.search_first_nick_name(
+            test_players_no_dupes, "shouRInji")
+        assert isinstance(matches, list)
+        assert len(matches) == 1
+        assert matches[0] == test_players_no_dupes[0]
+
+    @staticmethod
+    def test_search_first_two(test_players_same_first):
+        """ Tests whether a list with the correct Player objects is returned 
+            for a matching first name if more than 1 matches are found. """
+        matches = Utility.search_first_nick_name(
+            test_players_same_first, "shourinji")
+        assert isinstance(matches, list)
+        assert len(matches) == 2
+        assert matches[0] == test_players_same_first[0]
+        assert matches[1] == test_players_same_first[1]
+
+    @staticmethod
+    def test_search_nick(test_players_no_dupes):
+        """ Tests whether a list with the correct Player object 
+            is returned for a matching nickname. """
+        matches = Utility.search_first_nick_name(
+            test_players_no_dupes, "shourin")
+        assert isinstance(matches, list)
+        assert len(matches) == 1
+        assert matches[0] == test_players_no_dupes[0]
+
+    @staticmethod
+    def test_search_nick_case(test_players_no_dupes):
+        """ Tests whether a list with the correct Player object is 
+            returned for a matching nickname regardless of case. """
+        matches = Utility.search_first_nick_name(
+            test_players_no_dupes, "shouRIn")
+        assert isinstance(matches, list)
+        assert len(matches) == 1
+        assert matches[0] == test_players_no_dupes[0]
+
+    @staticmethod
+    def test_search_nick_two(test_players_same_nick):
+        """ Tests whether a list with the correct Player objects is returned 
+            for a matching nickname if more than 1 matches are found. """
+        matches = Utility.search_first_nick_name(
+            test_players_same_nick, "shourin")
+        assert isinstance(matches, list)
+        assert len(matches) == 2
+        assert matches[0] == test_players_same_nick[0]
+        assert matches[1] == test_players_same_nick[1]
+
+    @staticmethod
+    def test_search_last(test_players_no_dupes):
+        """ Tests whether a list with the correct Player object 
+            is returned for a matching last name. """
+        matches = Utility.search_last_name(
+            test_players_no_dupes, "sakichi")
+        assert isinstance(matches, list)
+        assert len(matches) == 1
+        assert matches[0] == test_players_no_dupes[1]
+
+    @staticmethod
+    def test_search_last_case(test_players_no_dupes):
+        """ Tests whether a list with the correct Player object is 
+            returned for a matching last name regardless of case. """
+        matches = Utility.search_last_name(
+            test_players_no_dupes, "SaKichi")
+        assert isinstance(matches, list)
+        assert len(matches) == 1
+        assert matches[0] == test_players_no_dupes[1]
+
+    @staticmethod
+    def test_search_last_two(test_players_same_last):
+        """ Tests whether a list with the correct Player objects is returned 
+            for a matching last name if more than 1 matches are found. """
+        matches = Utility.search_last_name(
+            test_players_same_last, "ayumu")
+        assert isinstance(matches, list)
+        assert len(matches) == 2
+        assert matches[0] == test_players_same_last[0]
+        assert matches[1] == test_players_same_last[1]
